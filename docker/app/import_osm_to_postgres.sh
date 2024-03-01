5d794384882f395c40622be8d418364ad4e43824
MikhalevMakar <mikhalevmakar@mail.ru>
2024-03-01 17:26:12 +0700

    add all

--- docker/app/import_osm_to_postgres.sh
+++ docker/app/import_osm_to_postgres.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Скачиваем небольшой файл .osm.pbf
-wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf
+wget ${HOST_INFO}
 
-# Выполняем запрос к osmosis для фильтрации данных
-osmosis \
-    --read-pbf file="monaco-latest.osm.pbf" \
-    --bounding-box top=43.7512 left=7.4061 bottom=43.7235 right=7.4395 \
-    --write-pgsql host="river_knowledge_postgres" database="backend" user="postgres" password="postgres"
-
-echo "Импорт в базу данных успешно завершен."
+imposm import -config config.json -mapping mapping.json -read ${RESULT_INFO_FILE} -overwritecache -write -diff
--- docker/initdb/database.sql
+++ docker/initdb/database.sql
@@ -1,7 +1,26 @@
-DO $$ BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'backend') THEN
-        CREATE DATABASE backend;
-    END IF;
-END $$;
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'backend') THEN
+            CREATE DATABASE backend;
+        END IF;
+    END
+$$;
 
-grant all privileges on database backend to postgres;
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE TABLE points
+(
+    osm_id   BIGINT PRIMARY KEY,
+    geometry GEOMETRY(Point, 4326)
+);
+
+CREATE INDEX idx_points_geom ON points USING GIST (geometry);
+
+CREATE TABLE lines
+(
+    osm_id   BIGINT PRIMARY KEY,
+    geometry GEOMETRY(LineString, 4326)
+);
+
+CREATE INDEX idx_lines_geom ON lines USING GIST (geometry);
