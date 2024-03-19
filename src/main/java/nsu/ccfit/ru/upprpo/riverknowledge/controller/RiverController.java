b9b43f965f7630ae2c39cb803fde441b9931f91a
Kirill Bolotov <k.y.bolotov@gmail.com>
2024-03-19 14:00:26 +0700

    Исправил RequestBody на PathVariable для контроллеров. Добавил базовую валидацию

--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/controller/RiverController.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/controller/RiverController.java
@@ -1,29 +1,34 @@
 package nsu.ccfit.ru.upprpo.riverknowledge.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverEntity;
-import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverName;
 import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.geojson.result.GeoServiceResult;
 import nsu.ccfit.ru.upprpo.riverknowledge.service.RiverService;
 import nsu.ccfit.ru.upprpo.riverknowledge.service.geojson.GeoFeaturesService;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/river/")
+@RequestMapping(value = "/api/river/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RiverController {
     private final RiverService riverService;
     private final GeoFeaturesService geoFeaturesService;
 
-    @GetMapping("search")
-    public List<RiverEntity> getRivers(@RequestBody RiverName riverName) {
-        return riverService.getRiver(riverName.getName());
+    @GetMapping("search/{name}")
+    public List<RiverEntity> getRivers(@PathVariable(value = "name")
+                                       @NotNull @NotBlank @Length(min=2) String riverName) {
+        return riverService.getRiver(riverName);
     }
 
-    @GetMapping("geo/info")
-    public GeoServiceResult getRiverGeoInfo(@RequestBody RiverName riverName) {
-        return geoFeaturesService.getRiverInfoFromGeoJson(riverName.getName());
+    @GetMapping("geo/info/{name}")
+    public GeoServiceResult getRiverGeoInfo(@PathVariable(value = "name")
+                                            @NotNull @NotBlank @Length(min=2) String riverName) {
+        return geoFeaturesService.getRiverInfoFromGeoJson(riverName);
     }
 }
