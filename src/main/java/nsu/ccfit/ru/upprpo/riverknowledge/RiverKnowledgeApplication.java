8129410630a546870f7f023eef5bebcfde636c61
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-19 15:23:28 +0300

    Убран конфликт

--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/RiverKnowledgeApplication.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/RiverKnowledgeApplication.java
@@ -4,15 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<< HEAD:src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/RiverknowledgeApplication.java
-public class RiverknowledgeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(RiverknowledgeApplication.class, args);
-    }
-=======
 public class RiverKnowledgeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RiverKnowledgeApplication.class, args);
 	}
->>>>>>> develop:src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/RiverKnowledgeApplication.java
 }
