dfe1e1c19a8d60ff99118dc1b3166c290686a8f1
Kirill Bolotov <k.y.bolotov@gmail.com>
2024-03-19 14:14:53 +0700

    Изменил названия атрибутов в JSON-ке результата геометрической обработки

--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/model/entity/geojson/result/DistancesFromRiverMouth.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/model/entity/geojson/result/DistancesFromRiverMouth.java
@@ -1,5 +1,6 @@
 package nsu.ccfit.ru.upprpo.riverknowledge.model.entity.geojson.result;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class DistancesFromRiverMouth {
+    @JsonProperty(value = "source_river")
     private final String sourceRiverName;
+    @JsonProperty(value = "inflow")
     private final String inflowName;
+    @JsonProperty(value = "distance_from_mouth_km")
     private final double distanceFromMouthKm;
 }
--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/model/entity/geojson/result/GeoServiceResult.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/model/entity/geojson/result/GeoServiceResult.java
@@ -1,5 +1,6 @@
 package nsu.ccfit.ru.upprpo.riverknowledge.model.entity.geojson.result;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,8 +11,10 @@ import java.util.List;
 @Setter
 @RequiredArgsConstructor
 public class GeoServiceResult {
+    @JsonProperty(value = "inflows_count")
     private final int inflowsCount;
+    @JsonProperty(value = "length_km")
     private final double riverLengthKm;
+    @JsonProperty(value = "distances_to_inflows")
     private final List<DistancesFromRiverMouth> distancesList;
-
 }
