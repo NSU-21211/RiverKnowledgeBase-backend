94b1336a87bce0ceeef88a0d35609649e40933f3
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-22 16:52:39 +0300

    Изменение аргументов контроллера

--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/controller/RiverController.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/controller/RiverController.java
@@ -1,21 +1,22 @@
 package nsu.ccfit.ru.upprpo.riverknowledge.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import nsu.ccfit.ru.upprpo.riverknowledge.model.dto.RiverDTO;
 import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverEntity;
-import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverName;
 import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.geojson.result.GeoServiceResult;
 import nsu.ccfit.ru.upprpo.riverknowledge.service.RiverService;
 import nsu.ccfit.ru.upprpo.riverknowledge.service.geojson.GeoFeaturesService;
+import nsu.ccfit.ru.upprpo.riverknowledge.util.RiverPairKey;
+import org.hibernate.validator.constraints.Length;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,15 +24,17 @@ import java.util.stream.Collectors;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/river")
+//@RequestMapping(value = "/api/river/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RiverController {
     private final RiverService riverService;
     private final GeoFeaturesService geoFeaturesService;
     private final ModelMapper mapper;
 
-    @GetMapping("/search")
-    public String getRivers(@RequestBody RiverName riverName, Model model) {
-        Map<URI, RiverEntity> rivers = riverService.getRiverInfo(riverName.getName());
-        Map<URI, RiverDTO> riverDTOMap = rivers.entrySet().stream()
+    @GetMapping("/search/{name}")
+    public String getRivers(@PathVariable(value = "name")
+                            @NotNull @NotBlank @Length(min = 2) String riverName, Model model) {
+        Map<RiverPairKey, RiverEntity> rivers = riverService.getRiverInfo(riverName);
+        Map<RiverPairKey, RiverDTO> riverDTOMap = rivers.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> mapper.map(entry.getValue(), RiverDTO.class)
@@ -40,15 +43,17 @@ public class RiverController {
         return "river_list";
     }
 
-    @GetMapping("/get")
-    public String getRiver(@RequestParam("river") String riverName, Model model) {
+    @GetMapping("/get/{name}")
+    public String getRiver(@PathVariable(value = "name")
+                           @NotNull @NotBlank @Length(min = 2) String riverName, Model model) {
         Optional<RiverEntity> river = riverService.getRiverByName(riverName);
         model.addAttribute("river", river);
         return "river_details";
     }
 
-    @GetMapping("geo/info")
-    public GeoServiceResult getRiverGeoInfo(@RequestBody RiverName riverName) {
-        return geoFeaturesService.getRiverInfoFromGeoJson(riverName.getName());
+    @GetMapping("geo/info/{name}")
+    public GeoServiceResult getRiverGeoInfo(@PathVariable(value = "name")
+                                            @NotNull @NotBlank @Length(min = 2) String riverName) {
+        return geoFeaturesService.getRiverInfoFromGeoJson(riverName);
     }
 }
