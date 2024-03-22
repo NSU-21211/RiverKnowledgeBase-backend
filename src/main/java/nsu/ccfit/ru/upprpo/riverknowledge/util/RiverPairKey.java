18ed474db4dd11154299c14b537d5ae688f8b040
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-22 15:17:07 +0300

    Добавлен класс для парса результата

---
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/util/RiverPairKey.java
@@ -0,0 +1,29 @@
+package nsu.ccfit.ru.upprpo.riverknowledge.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.net.URI;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RiverPairKey {
+    URI link;
+    String label;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RiverPairKey key = (RiverPairKey) o;
+        return link.equals(key.getLink()) && label.equals(key.getLabel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(link, label);
+    }
+}
