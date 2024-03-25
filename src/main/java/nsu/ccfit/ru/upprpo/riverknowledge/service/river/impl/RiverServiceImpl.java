b9eafaefd41cbf0c991c4e440757adad1a4662d5
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-26 00:17:25 +0300

    Вынесение логики по запросам в новый сервис

---
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/service/river/impl/RiverServiceImpl.java
@@ -0,0 +1,56 @@
+package nsu.ccfit.ru.upprpo.riverknowledge.service.river.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.dto.RiverDTO;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverEntity;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.wikidata.query.WikidataQuery;
+import nsu.ccfit.ru.upprpo.riverknowledge.service.query.QueryService;
+import nsu.ccfit.ru.upprpo.riverknowledge.service.river.RiverService;
+import nsu.ccfit.ru.upprpo.riverknowledge.util.RiverPairKey;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RiverServiceImpl implements RiverService {
+    private final Map<RiverPairKey, RiverEntity> rivers = new ConcurrentHashMap<>();
+    private final QueryService queryService;
+    private final List<WikidataQuery> wikidataQueryList;
+    private final ModelMapper mapper;
+
+    @Override
+    public List<RiverDTO> getRiverInfo(String name) {
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        queryService.riverLabelAndURIQuery(name, rivers);
+
+        for (WikidataQuery query : wikidataQueryList) {
+            CompletableFuture<Void> future = queryService.executeQuery(name, rivers, query);
+            futures.add(future);
+        }
+
+        CompletableFuture<Void> allDone = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allDone.join();
+
+        return rivers.values().stream()
+                .map(entity -> mapper.map(entity, RiverDTO.class))
+                .toList();
+    }
+
+
+    @Override
+    public Optional<RiverEntity> getRiverByName(String name) {
+        return rivers.values().stream()
+                .filter(river -> river.getLabel().equals(name))
+                .findFirst();
+    }
+
+}
