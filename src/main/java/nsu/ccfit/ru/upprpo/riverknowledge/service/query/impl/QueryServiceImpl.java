6801865eae3162c756758aef60b00fd475329fed
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-26 00:17:51 +0300

    Реализация сервиса для вызова запросов

---
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/service/query/impl/QueryServiceImpl.java
@@ -0,0 +1,94 @@
+package nsu.ccfit.ru.upprpo.riverknowledge.service.query.impl;
+
+import com.bordercloud.sparql.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.entity.RiverEntity;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.wikidata.parser.WikidataResponseParser;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.wikidata.parser.impl.URIAndLabelParser;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.wikidata.query.WikidataQuery;
+import nsu.ccfit.ru.upprpo.riverknowledge.model.wikidata.query.impl.RiverURIAndLabelQuery;
+import nsu.ccfit.ru.upprpo.riverknowledge.service.query.QueryService;
+import nsu.ccfit.ru.upprpo.riverknowledge.util.RiverPairKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class QueryServiceImpl implements QueryService {
+    private final SparqlClient client = new SparqlClient(true);
+    private final List<WikidataResponseParser> parserList;
+
+    @Value(value = "${wikidata.endpoint}")
+    private String wikidataEndpoint;
+
+    @Override
+    public void riverLabelAndURIQuery(String name, Map<RiverPairKey, RiverEntity> rivers) {
+        client.setEndpointRead(URI.create(wikidataEndpoint));
+        client.setMethodHTTPRead(Method.GET);
+
+        RiverURIAndLabelQuery query = new RiverURIAndLabelQuery();
+        String querySelect = query.getWikidataQuery(name);
+
+        try {
+            SparqlResult result = client.query(querySelect);
+            SparqlResultModel resultModel = result.getModel();
+            log.info("Query " + query.getType() + " for river " + name + " done. Row count = " + resultModel.getRowCount());
+
+            if (resultModel.getRowCount() > 0) {
+                URIAndLabelParser parser = new URIAndLabelParser();
+                parser.parse(resultModel, rivers);
+                log.info(parser.getType() + " parse has finished. Rivers size = " + rivers.size());
+            } else {
+                log.warn("No information for river " + name);
+            }
+        } catch (SparqlClientException e) {
+            log.error("Error executing request SPARQL: {}", (Object) e.getSuppressed());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public CompletableFuture<Void> executeQuery(String name, Map<RiverPairKey, RiverEntity> rivers, WikidataQuery query) {
+        String querySelect = query.getWikidataQuery(name);
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return client.query(querySelect);
+            } catch (SparqlClientException e) {
+                log.error("Error executing request SPARQL for " + query.getType() + ": {}", e.getMessage());
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }).thenAccept(result -> {
+            SparqlResultModel resultModel = result.getModel();
+            log.info("Query " + query.getType() + " for river " + name + " done. Row count = " + resultModel.getRowCount());
+
+            if (resultModel.getRowCount() > 0) {
+                Optional<WikidataResponseParser> parser = getParserByType(query.getType());
+                if (parser.isPresent()) {
+                    parser.get().parse(resultModel, rivers);
+                    log.info(parser.get().getType() + " parse has finished. Rivers size = " + rivers.size());
+                } else {
+                    log.error("No parser for " + query.getType() + " query");
+                }
+            } else {
+                log.warn("No information for river " + name);
+            }
+        });
+    }
+
+    private Optional<WikidataResponseParser> getParserByType(String type) {
+        return parserList.stream()
+                .filter(parser -> parser.getType().equals(type))
+                .findFirst();
+    }
+
+}
