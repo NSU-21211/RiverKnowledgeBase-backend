e7b5a9bdd918208474ae117973b809a286dbade5
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-03-28 19:25:48 +0300

    Добавление аннотации EqualsAndHashCode

--- src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/util/RiverPairKey.java
+++ src/main/java/nsu/ccfit/ru/upprpo/riverknowledge/util/RiverPairKey.java
@@ -1,29 +1,17 @@
 package nsu.ccfit.ru.upprpo.riverknowledge.util;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.net.URI;
-import java.util.Objects;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class RiverPairKey {
     private URI link;
     private String label;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RiverPairKey key = (RiverPairKey) o;
-        return link.equals(key.getLink()) && label.equals(key.getLabel());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(link, label);
-    }
 }
